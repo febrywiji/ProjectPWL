38d7b5b7db21fa626354fe2f56909080d26a681e
Febrywiji <febrywiji12@gmail.com>
2024-03-13 21:08:55 +0700

    Delete Ebook.php

--- app/Models/Ebook.php
+++
@@ -1,8 +0,0 @@
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Ebook extends Model
-{
-    protected $fillable = ['title', 'author', 'description', 'cover_image', 'file'];
-}
